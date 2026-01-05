d049f9dde904f23bb2a371828e574ed200bd6f0a
Alex Scott <codetheorist@outlook.com>
2026-01-05 13:46:51 +0000

    chore: tooling config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
 // @ts-check
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
-export default createConfigForNuxt({
-  features: {
-    // Rules for module authors
-    tooling: true,
-    // Rules for formatting
-    stylistic: true,
+export default createConfigForNuxt(
+  {
+    features: {
+      // Rules for module authors
+      tooling: true,
+      // Rules for formatting
+      stylistic: false,
+    },
+    dirs: {
+      src: ["./playground"],
+    },
   },
-  dirs: {
-    src: [
-      './playground',
-    ],
+  {
+    rules: {
+      "no-console": "off", // allow console.log in TypeScript files
+    },
+  }
+).override("nuxt/typescript", {
+  rules: {
+    // ...Override rules, for example:
+    "@typescript-eslint/ban-types": "off",
   },
-})
-  .append(
-    // your custom flat config here...
-  )
+});
