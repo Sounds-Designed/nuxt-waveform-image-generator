0919a1283b6c51189ebe60da61172dd064f35d2b
Alex Scott <codetheorist@outlook.com>
2026-01-09 18:11:53 +0000

    fix: linear path type schema

--- src/runtime/schemas/LinearWaveformComponentPropsSchema.ts
+++ src/runtime/schemas/LinearWaveformComponentPropsSchema.ts
@@ -3,7 +3,8 @@ import type { ILinearWaveformComponentProps } from "../types";
 import BaseWaveformComponentPropsSchema from "./BaseWaveformComponentPropsSchema";
 
 export const LinearWaveformComponentPropsSchema = BaseWaveformComponentPropsSchema.extend({
-
+  height: z.number().positive(),
+  width: z.number().positive(),
 }) satisfies z.ZodType<ILinearWaveformComponentProps>;
 
 export default LinearWaveformComponentPropsSchema;
