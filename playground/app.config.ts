f3e08305e47184cc55398230b71f7c2cbde27850
Alex Scott <codetheorist@outlook.com>
2026-01-09 18:07:26 +0000

    fix: playground typescript issues

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -3,7 +3,10 @@ export default defineAppConfig({
   appName: "Waveform Image Generator Playground",
   ui: {
     main: {
-      base: "min-h-[calc(100vh-calc(calc(var(--spacing)*27)+var(--ui-header-height)))] lg:min-h-[calc(100vh-calc(calc(var(--spacing)*13)+var(--ui-header-height)))]",
+      base: "min-h-[calc(100vh-calc(calc(var(--spacing)*27)+var(--ui-header-height)))] lg:min-h-[calc(100vh-calc(56px+var(--ui-header-height)))]",
     },
+    pageBody: {
+      base: 'mt-8 pb-4 space-y-12'
+    }
   },
 });
