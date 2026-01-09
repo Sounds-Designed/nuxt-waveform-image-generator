9b0324f7542c55604db7c21c609d963884d49ccd
Alex Scott <codetheorist@outlook.com>
2026-01-09 11:55:33 +0000

    chore: remove useless comment

--- src/module.ts
+++ src/module.ts
@@ -19,7 +19,6 @@ export default defineNuxtModule<WaveformImageGeneratorOptions>({
   setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
     addComponent({
       name: 'LinearWaveformImage',
